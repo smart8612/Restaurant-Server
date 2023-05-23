c671af348571158c2a5b4e1d67d913c342b98849
JeongTaek Han <smart8612@gmail.com>
2023-05-23 15:23:49 +0900

    feat: Implement Category Response

---
+++ Sources/App/Models/Contents/Categories/Response/CategoryResponse.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryResponse.swift
+//  
+//
+//  Created by JeongTaek Han on 2023/05/23.
+//
+
+
+import Fluent
+import Vapor
+
+
+struct CategoryResponse: Content {
+    
+    var id: Int
+    var name: String
+    
+}
+
